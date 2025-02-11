5457e27bb1b482a6323d40ad776cdc10a39e8ecf
Rafael Lauton <rafalauton@gmail.com>
2025-02-11 00:08:22 -0300

    prepara o ambiente
    # Conflicts:
    #	README.md

---
+++ src/main/java/com/farmacia/pharma_manager/PharmaManagerApplication.java
@@ -0,0 +1,13 @@
+package com.farmacia.pharma_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmacia/pharma_manager/PharmaManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.pharma_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
