0fd930d91cbd560acfc6d050fb3f33cdb5cb333b
Manoelmonteiro <manoell2002@gmail.com>
2025-03-12 21:41:20 -0300

    adicao de metodos

--- src/main/cadastros/ControleFuncionario.java
+++ src/main/cadastros/ControleFuncionario.java
@@ -6,13 +6,31 @@ public class ControleFuncionario {
 
       // Construtor
       public ControleFuncionario() {
-        funcionarios = new ArrayList<>();
+        listaFuncionarios = new ArrayList<>();
     }
 
-    public void cadastrarFuncionario (funcionario Funcionario){
+    public void cadastrarFuncionario (Funcionario funcionario){
         listaFuncionarios.add(funcionario);
+        System.out.println("Funcionário cadastrado com sucesso: " + funcionario.getNome());
     }
 
-    public void alterarFuncionario ()
+    public Funcionario consultarFuncionario(String cpf) {
+        for (Funcionario f : funcionarios) {
+            if (f.getCpf().equals(cpf)) {
+                return f;
+            }
+        }
+    }
+
+    public void alterarFuncionario(String cpf, String novoNome, String novoCargo) {
+        Funcionario funcionario = consultarFuncionario(cpf);
+        if (funcionario != null) {
+            funcionario.setNome(novoNome);
+            funcionario.setCargo(novoCargo);
+            System.out.println("Funcionário alterado com sucesso: " + novoNome);
+        } else {
+            System.out.println("Funcionário não encontrado.");
+        }
+    }
     
 }
