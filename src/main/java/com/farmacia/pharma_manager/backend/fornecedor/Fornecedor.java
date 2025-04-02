b61af2a6ede2b4e6dd6a6b7bdd7fa4636690dc97
williamsilvaj <willian2014s24c@gmail.com>
2025-04-02 00:20:51 -0300

    Gerenciamento de itens adicionado

--- src/main/java/com/farmacia/pharma_manager/backend/fornecedor/Fornecedor.java
+++ src/main/java/com/farmacia/pharma_manager/backend/fornecedor/Fornecedor.java
@@ -28,52 +28,51 @@ public class Fornecedor {
     @JoinColumn(name = "idEndereco", nullable = false)
     private Endereco endereco;
 
-    // Getters e Setters
-    public Integer getIdFornecedor() {
-        return idFornecedor;
-    }
+  public Integer getIdFornecedor() {
+    return idFornecedor;
+  }
 
-    public void setIdFornecedor(Integer idFornecedor) {
-        this.idFornecedor = idFornecedor;
-    }
+  public void setIdFornecedor(Integer idFornecedor) {
+    this.idFornecedor = idFornecedor;
+  }
 
-    public String getNome() {
-        return nome;
-    }
+  public String getNome() {
+    return nome;
+  }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 
-    public String getCnpj() {
-        return cnpj;
-    }
+  public String getCnpj() {
+    return cnpj;
+  }
 
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public Integer getStatus() {
-        return status;
-    }
+  public Integer getStatus() {
+    return status;
+  }
 
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
+  public void setStatus(Integer status) {
+    this.status = status;
+  }
 
-    public Endereco getEndereco() {
-        return endereco;
-    }
+  public Endereco getEndereco() {
+    return endereco;
+  }
 
-    public void setEndereco(Endereco endereco) {
-        this.endereco = endereco;
-    }
+  public void setEndereco(Endereco endereco) {
+    this.endereco = endereco;
+  }
 }
--- src/main/java/com/farmacia/pharma_manager/backend/funcionario/FuncionarioRepository.java
+++ src/main/java/com/farmacia/pharma_manager/backend/funcionario/FuncionarioRepository.java
@@ -1,8 +1,12 @@
 package com.farmacia.pharma_manager.backend.funcionario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+@Query("SELECT f FROM Funcionario f WHERE f.idFuncionario = :id")
+Funcionario buscarPorId(@Param("id") Integer id);
 }
